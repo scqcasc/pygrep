f3ddc0411de5a25afddc17ce651da9e73097e9e6
Sean Quaint <seanquaint@zohomail.com>
2025-06-29 21:27:57 -0400

    catch errors on opening files

--- libs.py
+++ libs.py
@@ -1,3 +1,4 @@
+import sys
 
 COLOURS = {}
 COLOURS["RED"] = "\033[31m"
@@ -9,22 +10,26 @@ COLOURS["RESET"] = "\033[0m"
 
 def grep(pattern: list, file_name: str, nc: bool, word: bool = False) -> list:
     lines = []
-    with open(file_name, "r") as f:
-        x = 1         # x is the line number which gets added to the grepped data
-        for line in f.readlines():
-            line = line.strip()
-            for p in pattern:
-                if word:
-                    # if word option is used the surround the pattern with white space
-                    p = f' {p} '
-                if p in line:
-                    if not nc:
-                        line = line.replace(
-                            p, f"{COLOURS['GREEN']}{p}{COLOURS['RESET']}"
-                        )
-                    lines.append((x, line))
-            x = x + 1
-    return lines
+    try:
+        with open(file_name, "r") as f:
+            x = 1         # x is the line number which gets added to the grepped data
+            for line in f.readlines():
+                line = line.strip()
+                for p in pattern:
+                    if word:
+                        # if word option is used the surround the pattern with white space
+                        p = f' {p} '
+                    if p in line:
+                        if not nc:
+                            line = line.replace(
+                                p, f"{COLOURS['GREEN']}{p}{COLOURS['RESET']}"
+                            )
+                        lines.append((x, line))
+                x = x + 1
+        return lines
+    except Exception as e:
+        print(f"Error dealing with file {file_name}: {e}")
+        sys.exit(1)
 
 
 def display(results: list, ln: bool) -> None:
